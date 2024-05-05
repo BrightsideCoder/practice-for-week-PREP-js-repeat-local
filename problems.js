60b6aa2f81aff434f145cb2b1ffe7f3074f007f2
Torsten Krüger <torsten.kruger90@gmail.com>
2024-05-05 09:25:12 +0200

    Finished 8th problem

--- problems.js
+++ problems.js
@@ -62,6 +62,13 @@ function fizzBuzz(max) {
     /* Takes a number, max and returns an array that contains every number from
     0 to max (not inclusive) that is divisible by either 3 or 5, **but not both**. */
     // Your code here
+    let newArr = [];
+    for (let i = 0; i < max; i++) {
+        if ( (i % 3 != 0 && i % 5 == 0) || (i % 3 == 0 && i % 5 != 0) ) {
+            newArr.push(i);
+        }  
+    };
+    return newArr;
 };
 
 function hello(name) {
